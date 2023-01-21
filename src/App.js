239462472a657462946cd2c4e68b88b7d12e6b68
zaki <92805900+zaki164@users.noreply.github.com>
2023-01-21 19:52:13 +0200

    add suspense and comploader

--- src/App.js
+++ src/App.js
@@ -1,24 +1,10 @@
-import { HashRouter as Router, Route, Routes } from 'react-router-dom';
-import { Home, Navbar } from './components';
-import { Articles, Features, Gallery, HowItWorks, Pricing, Services, Skills, TeamMembers, Testimonials, TopVideos } from './Sections';
+import { HashRouter as Router } from 'react-router-dom';
+import Layout from './Layout/Layout';
 import './App.css';
 
 const App = () => (
   <Router>
-    <Navbar />
-    <Routes>
-      <Route exact path='/' element={<Home />} />
-      <Route path='/Gallery' element={<Gallery />} />
-      <Route path='/Features' element={<Features />} />
-      <Route path='/Testimonials' element={<Testimonials />} />
-      <Route path='/Articles' element={<Articles />} />
-      <Route path='/Skills' element={<Skills />} />
-      <Route path='/Pricing' element={<Pricing />} />
-      <Route path='/HowItWorks' element={<HowItWorks />} />
-      <Route path='/TeamMembers' element={<TeamMembers />} />
-      <Route path='/Services' element={<Services />} />
-      <Route path='/TopVideos' element={<TopVideos />} />
-    </Routes>
+    <Layout />
   </Router>
 );
 
